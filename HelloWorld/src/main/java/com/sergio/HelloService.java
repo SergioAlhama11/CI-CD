e0f772c18c73fef3fe1cc61ef342ec8031f5bb12
Sergio Alhama <sergio.alhama.11@gmail.com>
2025-05-27 11:01:55 +0200

    First Commit

---
+++ HelloWorld/src/main/java/com/sergio/HelloService.java
@@ -0,0 +1,14 @@
+package com.sergio;
+
+import io.quarkus.grpc.
+import io.smallrye.mutiny.Uni;
+
+@GrpcService
+public class HelloService implements Greeter {
+
+    @Override
+    public Uni<HelloReply> implements sayHello() {
+
+    }
+
+}
