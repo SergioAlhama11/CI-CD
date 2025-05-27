d913f451d7e64d8460ca43d6d1d36bd82c149255
Sergio Alhama <sergio.alhama.11@gmail.com>
2025-05-27 12:06:00 +0200

    Adding changes

--- HelloWorld/src/main/java/com/sergio/HelloService.java
+++ HelloWorld/src/main/java/com/sergio/HelloService.java
@@ -1,14 +1,23 @@
-package com.sergio;
+package io.quarkus.grpc.examples.hello;
 
-import io.quarkus.grpc.
+import java.util.concurrent.atomic.AtomicInteger;
+
+import examples.Greeter;
+import examples.HelloReply;
+import examples.HelloRequest;
+import io.quarkus.grpc.GrpcService;
 import io.smallrye.mutiny.Uni;
 
 @GrpcService
-public class HelloService implements Greeter {
+public class HelloWorldService implements Greeter {
+
+    AtomicInteger counter = new AtomicInteger();
 
     @Override
-    public Uni<HelloReply> implements sayHello() {
-
+    public Uni<HelloReply> sayHello(HelloRequest request) {
+        int count = counter.incrementAndGet();
+        String name = request.getName();
+        return Uni.createFrom().item("Hello " + name)
+                .map(res -> HelloReply.newBuilder().setMessage(res).setCount(count).build());
     }
-
 }
--- Jenkinsfile
+++ Jenkinsfile
@@ -5,18 +5,28 @@ pipeline {
         stage('Build'){
             steps {
                 echo 'Construyendo la app...'
+                git 'https://github.com/SergioAlhama11/CI-CD.git'
             }
         }
 
         stage('Test') {
             steps {
                 echo 'Ejecutando tests...'
+                sh 'mvn clean package -DskipTests'
             }
         }
 
         stage('Deploy'){
             steps {
                 echo 'Desplegando app en Raspberry Pi...'
+                
+                sh '''
+                if lsof -ti:8080 | grep -q .; then
+                    kill $(lsof -ti:8080)
+                fi
+                '''
+
+                sh 'nohup java -jar target/quarkus-app/quarkus-run.jar > app.log 2>&1 &'
             }
         }
     }
